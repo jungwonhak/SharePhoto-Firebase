f18b4673ff40b67583cd1d9e68a8fcdbb707861b
Emine İnan <emineinanx@gmail.com>
2021-02-12 17:05:33 +0300

    Add menu

--- app/src/main/java/com/example/sharephoto/FeedActivity.kt
+++ app/src/main/java/com/example/sharephoto/FeedActivity.kt
@@ -2,10 +2,17 @@ package com.example.sharephoto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
