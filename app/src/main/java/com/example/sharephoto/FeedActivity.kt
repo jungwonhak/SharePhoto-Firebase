f4263463734ed2bd89d3343ef522a2b651f57329
Emine İnan <emineinanx@gmail.com>
2021-03-05 19:51:36 +0300

    Get data from Firestore

--- app/src/main/java/com/example/sharephoto/FeedActivity.kt
+++ app/src/main/java/com/example/sharephoto/FeedActivity.kt
@@ -5,15 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var database:FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
 
         auth=FirebaseAuth.getInstance()
+        database= FirebaseFirestore.getInstance()
+        getData()
+    }
+
+    fun getData(){
+        database.collection("Post").addSnapshotListener { value, error ->
+            if(error!=null){
+                Toast.makeText(this,error.localizedMessage,Toast.LENGTH_LONG).show()
+            }else{
+                if(value!=null){
+                    if(!value.isEmpty){
+                        val documents=value.documents
+                        for(document in documents){
+                            val userEmail=document.get("useremail")
+                            println(userEmail)
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
